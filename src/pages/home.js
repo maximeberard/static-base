07d271c36ebfeb5e4c64a04d736a38f1058452db
Maxime Bérard <contact@maximeberard.com>
2019-07-08 11:49:20 +1200

    Added default js page

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,14 +1,25 @@
 /**
- * Copyright Maxime Bérard 2016
+ * Copyright © 2016, Maxime Bérard
  *
  * @file home.js
  * @copyright Maxime Bérard 2016
  * @author Maxime Bérard
  */
-import {AbstractPage} from "abstract-page";
 
-export class Home extends AbstractPage {
-    constructor(router, id, context, type, isHome){
-        super(router, id, context, type, isHome);
+import {AbstractPage} from "abstract-page";
+// import {DefaultPage} from "pages/default-page";
+
+export class Home extends AbstractPage { // extends DefaultPage
+
+    init(){
+        super.init();
+    }
+
+    initEvents(){
+        super.initEvents();
+    }
+
+    destroyEvents(){
+        super.destroyEvents();
     }
 }
--- src/pages/page.js
+++ src/pages/page.js
@@ -1,14 +1,25 @@
 /**
- * Copyright Maxime Bérard 2016
+ * Copyright © 2016, Maxime Bérard
  *
  * @file page.js
  * @copyright Maxime Bérard 2016
  * @author Maxime Bérard
  */
-import {AbstractPage} from "abstract-page";
 
-export class Page extends AbstractPage {
-    constructor(router, id, context, type, isHome){
-        super(router, id, context, type, isHome);
+import {AbstractPage} from "abstract-page";
+// import {DefaultPage} from "pages/default-page";
+
+export class Page extends AbstractPage { // extends DefaultPage
+
+    init(){
+        super.init();
+    }
+
+    initEvents(){
+        super.initEvents();
+    }
+
+    destroyEvents(){
+        super.destroyEvents();
     }
 }
